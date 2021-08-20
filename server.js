886978d6bb901c57fba2b4fa41987f78d9e43bf4
Simon Koob <simon.koob@gmx.net>
2021-08-20 14:37:14 +0200

    further work on database

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const register = require('./controllers/register.js');
 const signin = require('./controllers/signin.js');
 const profile = require('./controllers/profile.js');
 const image = require('./controllers/image.js');
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
 
 const db = knex({
   client: 'pg',
