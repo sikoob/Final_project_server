4e0d01f9b7fb374681db4b2931e1a57d57bf22ce
Simon Koob <simon.koob@gmx.net>
2021-08-20 14:39:48 +0200

    changes database

--- server.js
+++ server.js
@@ -8,13 +8,14 @@ const register = require('./controllers/register.js');
 const signin = require('./controllers/signin.js');
 const profile = require('./controllers/profile.js');
 const image = require('./controllers/image.js');
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
 
 const db = knex({
   client: 'pg',
   connection: {
     connectionString : process.env.DATABASE_URL,
-    ssl:true
+    ssl: {
+    	rejectUnauthorized: false
+  	}
   }
 });
 
