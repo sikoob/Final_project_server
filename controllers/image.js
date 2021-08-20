678779c62404487062bd38619983b7e498e2f378
Simon Koob <simon.koob@gmx.net>
2021-08-20 12:40:07 +0200

    fixing security issues with Clarifai API key

--- controllers/image.js
+++ controllers/image.js
@@ -1,3 +1,19 @@
+const Clarifai = require('clarifai');
+
+/*using API Key in Backend to keep it secure*/
+const app = new Clarifai.App({
+ apiKey: '08a93ffa3c1d49fb9f137cc4123f6504'
+});
+
+const handleApiCall = (req, res) => {
+app.models
+	.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+	.then(data => {
+		res.json(data);
+	})
+	.catch(err => res.status(400).json('unable to activate API'))
+}
+
 const handleImagePut = (req, res, db) => {
 	const { id } = req.body;
 	db('users').where('id', '=', id)								/*Nur ein =, weil hier SQL Syntax*/
--- controllers/register.js
+++ controllers/register.js
@@ -1,5 +1,8 @@
 const handleRegister = (req, res, db, bcrypt) => {						/*Durch Verknüpfung in server.js können hier die entsprechendne Funktionen gerufen werden*/
 	const { email, name, password } = req.body;
+	if(!email || !name || !password) {
+		return res.status(400).json('inccorrect form submission');
+	}
 	const hash = bcrypt.hashSync(password);
 		db.transaction(trx => {											/*um Daten in zwei SQL Tabellen zu bringen*/
 			trx.insert({
